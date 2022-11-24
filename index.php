059571a21f44b76541eb445686a57b33a34e3928
lxcas <lucasgonzagasantos0201@gmail.com>
2022-11-24 19:08:43 -0300

    primeiro commit

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="index.css">
+    <script src="https://kit.fontawesome.com/dc9a30c3ce.js" crossorigin="anonymous"></script>
+    <title>Document</title>
+</head>
+<body>
+    <header>
+        <div class="logo">
+            <i class="fa-solid fa-egg"></i>
+        </div>
+        <div class="itens-menu">
+            <ul>
+                <li><i class="fa-solid fa-house"></i><a class="home">Pagina Inicial</a></li> 
+                <li><i class="fa-solid fa-hashtag"></i><a class="explorar">Explorar</a></li>
+                <li><i class="fa-solid fa-bell"></i><a class="noti">Notificações</a></li>
+                <li><i class="fa-solid fa-envelope"></i><a class="msg">Mensagens</a></li>
+                <li><i class="fa-solid fa-bookmark"></i><a class="itens">Itens Salvos</a></li>
+                <li><i class="fa-regular fa-newspaper"></i><a class="lista">Lista</a></li>
+                <li><i class="fa-solid fa-user"></i><a class="perfil">Perfil</a></li>
+                <li><i class="fa-solid fa-circle-chevron-down"></i><a class="mais">Mais</a></li>
+            </ul>
+        </div>
+    </header>
+</body>
+</html>
