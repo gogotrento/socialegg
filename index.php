8391aa77ad4a9272d1bb95ec4ad8201907d6aaf8
lxcas <lucasgonzagasantos0201@gmail.com>
2022-11-25 11:14:23 -0300

    Add eggtar e pesquisae

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="index.css">
     <script src="https://kit.fontawesome.com/dc9a30c3ce.js" crossorigin="anonymous"></script>
-    <title>Document</title>
+    <title>Social Egg</title>
 </head>
 <body>
     <header>
@@ -14,7 +14,7 @@
             <i class="fa-solid fa-egg"></i>
         </div>
         <div class="itens-menu">
-            <ul>
+            <ul class=menu-vertical>
                 <li><i class="fa-solid fa-house"></i><a class="home">Pagina Inicial</a></li> 
                 <li><i class="fa-solid fa-hashtag"></i><a class="explorar">Explorar</a></li>
                 <li><i class="fa-solid fa-bell"></i><a class="noti">Notificações</a></li>
@@ -23,8 +23,27 @@
                 <li><i class="fa-regular fa-newspaper"></i><a class="lista">Lista</a></li>
                 <li><i class="fa-solid fa-user"></i><a class="perfil">Perfil</a></li>
                 <li><i class="fa-solid fa-circle-chevron-down"></i><a class="mais">Mais</a></li>
+                <button type="submit" class="button-eggtar" style="width:12em; padding:15px; margin-top:5px; float:left;">Eggtar</button>
             </ul>
         </div>
     </header>
+    <section class="publicar-text">
+        <div class="container">
+            <div class="box">
+                <div class="texto">
+                    <input type="text" name="eggtar" id="eggtar" placeholder="Digite o que tem pra falar ai meu chapa">
+                    <button type="submit" class="button-eggtar">Eggtar</button>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="pesquisar">
+        <div class="container">
+            <div class="box-2">
+                
+                <input type="text" placeholder="&#128269; Pesquisar">
+            </div>
+        </div>
+    </section>
 </body>
 </html>
