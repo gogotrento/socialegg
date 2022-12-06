c7d10738b6dd94ed07dcdf385b08a0755901ef78
Matheus Torres <108310425+Pytheuz@users.noreply.github.com>
2022-12-06 11:43:39 -0300

    Update barragem-troca-senha.php

--- db/barragem-troca-senha.php
+++ db/barragem-troca-senha.php
@@ -1,7 +1,6 @@
 <?php
 
     include("conexao.php");
-    echo "<script>window.prompt('')</script>";
     echo "<noscript
     ><html><head><title>401 Authorization Required</title></head>
     <body>
@@ -11,16 +10,65 @@
 
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" href="l">
-        <link rel="icon" href="images/egg-solid.svg">
-        <script src="https://kit.fontawesome.com/dc9a30c3ce.js" crossorigin="anonymous"></script>
-        <title>Social Egg - Troca de senha</title>
-    </head>
 
-</html>
+<!DOCTYPE html>
+    <html lang='en'>
+    <center>
+        <head>
+        <script src='https://kit.fontawesome.com/dc9a30c3ce.js' crossorigin='anonymous'></script>
+        <script src='https://code.jquery.com/jquery-3.6.0.js'></script>
+        <script src='https://code.jquery.com/ui/1.13.2/jquery-ui.js'></script>
+        <link rel='stylesheet' href='barragem-reset-senha.css'>
+        <link rel='stylesheet' href='https://code.jquery.com/ui/1.13.2/themes/base/jquery-ui.css'>
+        <link rel='stylesheet' href='https://jqueryui.com/resources/demos/style.css'>
+        <title>Login - ADMIN</title>
+        <link rel='icon' href='.../images/egg-solid.svg'>
+            <script>
+                $( function() {
+                $( '#dialog' ).dialog();
+                } );
+            </script>
+        </head>
+        <body>
+            <div id='dialog' title='LOGIN'>
+                <form class='login' method='post' action='' autocomplete='off'>
+                    <p>Insira seu usuario e senha</p>
+                    <input class='usuario' name='usuario' placeholder='insira seu usuario' type='text'>
+                    <input class='senha' name='senha' placeholder='insira sua senha' type='password'>
+                    <br><button type='submit'>Login</button><br>
+                    <?php
+
+                        if(isset($_POST['usuario']) && isset($_POST['senha'])){
+
+                            $usuario = $_POST['usuario'];
+                            $senha = $_POST['senha'];
+
+
+                            if ($usuario=="" || $usuario==null || strlen($usuario) == 0){
+                                echo"<b style='color:red'>Campo usuário Inválido!</b>";
+                            
+                            }
+
+                            if ($senha=="" || $senha==null || strlen($senha) == 0){
+                                echo "<b style='color:red'>Campo senha Inválido!</b>";
+                            
+                            }
+                            
+                            if($usuario  != "adm"){
+                                echo "<b style='color:red'>Campo usuario Inválido!</b>";
+                            
+                            }
+                            
+                            if($senha != "helpdesk"){
+                                echo "<b style='color:red'>Campo senha Inválido!</b>";
+                            }
+                
+                            header("Location: ../troca-senha.php");
+                        }
+
+                    ?>
+                </form>
+            </div>
+        </body>
+    </center>
+    </html>
